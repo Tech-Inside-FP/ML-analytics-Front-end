c765c6b48aefdcbbdd68ea293064ed60a594d401
Thiago Amorim <thiago.amorimsilva38@gmail.com>
2023-04-15 19:19:12 -0300

    flutter initial config

---
+++ ml_analytics/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'ML-Analytics'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const SizedBox(height: 20),
+            SizedBox(
+              height: 50,
+              width: 160,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('Upload file'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
