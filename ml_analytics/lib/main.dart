3c20deda68977a8390483bf85f6ae2fd47b566e7
Thiago Amorim <thiago.amorimsilva38@gmail.com>
2023-04-25 02:09:21 -0300

    upload de xlsx e de csv

--- ml_analytics/lib/main.dart
+++ ml_analytics/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const UploadCsvScreen()
+      home: const UploadFileScreen()
     );
   }
 }
--- ml_analytics/lib/pages/upload_file_page.dart
+++ ml_analytics/lib/pages/upload_file_page.dart
@@ -1,18 +1,80 @@
+// ignore_for_file: avoid_print
+
+import 'dart:convert';
+import 'dart:typed_data';
+import 'package:csv/csv.dart';
 import 'package:excel/excel.dart';
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 
-class UploadCsvScreen extends StatefulWidget {
-  const UploadCsvScreen({super.key});
+class UploadFileScreen extends StatefulWidget {
+  const UploadFileScreen({Key? key}) : super(key: key);
 
   @override
-  UploadCsvScreenState createState() => UploadCsvScreenState();
+  UploadFileScreenState createState() => UploadFileScreenState();
 }
 
-class UploadCsvScreenState extends State<UploadCsvScreen> {
+class UploadFileScreenState extends State<UploadFileScreen> {
   String tableName = '';
   String maxCols = '';
-  String maxRowls = '';
+  String maxRows = '';
+
+  bool isUtf8(List<int> bytes) {
+    try {
+      utf8.decode(bytes);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  Future<void> _selectFile() async {
+    final pickedFiles = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['xlsx', 'csv'],
+      allowMultiple: false,
+    );
+
+    if (pickedFiles != null) {
+      final file = pickedFiles.files.single;
+
+      String? extension = file.extension;
+      Uint8List? bytes = file.bytes;
+
+      if (bytes != null) {
+        if (extension == 'xlsx') {
+          var excel = Excel.decodeBytes(bytes);
+          for (var table in excel.tables.keys) {
+            setState(() {
+              tableName = table;
+              maxCols = excel.tables[table]!.maxCols.toString();
+              maxRows = excel.tables[table]!.maxRows.toString();
+            });
+            break;
+          }
+        } else if (extension == 'csv') {
+          String csvString;
+          if (isUtf8(bytes)) {
+            csvString = utf8.decode(bytes);
+          } else {
+            csvString = const Utf8Decoder(allowMalformed: true).convert(bytes);
+          }
+
+          List<List<dynamic>> csvData =
+              const CsvToListConverter().convert(csvString);
+
+          setState(() {
+            tableName = 'CSV';
+            maxCols = csvData[0].length.toString();
+            maxRows = csvData.length.toString();
+          });
+        } else {
+          print('No valid extension or bytes is null.');
+        }
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,34 +86,22 @@ class UploadCsvScreenState extends State<UploadCsvScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-              onPressed: () async {
-                FilePickerResult? pickedFile =
-                    await FilePicker.platform.pickFiles(
-                  type: FileType.custom,
-                  allowedExtensions: ['xlsx'],
-                  allowMultiple: false,
-                );
-                if (pickedFile != null) {
-                  var bytes = pickedFile.files.single.bytes;
-                  var excel = Excel.decodeBytes(bytes!);
-                  for (var table in excel.tables.keys) {
-                    setState(() {
-                      tableName = table;
-                      maxCols = excel.tables[table]!.maxCols.toString();
-                      maxRowls = excel.tables[table]!.maxRows.toString();
-                    });
-                  }
-                }
-              },
-              child: const Text('Select XLSX File'),
+              onPressed: _selectFile,
+              child: const Text('Select XLSX or CSV File'),
             ),
             const SizedBox(height: 20),
-            // ignore: prefer_interpolation_to_compose_strings
-            Text('Nome da tabela: ' + tableName),
-            const SizedBox(height: 5),
-            Text('Quantidade de colunas: $maxCols'),
-            const SizedBox(height: 5),
-            Text('Quantidade de linhas: $maxRowls'),
+            Visibility(
+              visible: tableName.isNotEmpty,
+              child: Column(
+                children: [
+                  Text('Nome da tabela: $tableName'),
+                  const SizedBox(height: 5),
+                  Text('Quantidade de colunas: $maxCols'),
+                  const SizedBox(height: 5),
+                  Text('Quantidade de linhas: $maxRows'),
+                ],
+              ),
+            ),
           ],
         ),
       ),
