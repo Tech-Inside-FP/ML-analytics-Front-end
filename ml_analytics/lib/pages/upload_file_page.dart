d78e0e864535c9fb8b88bbfaa84d17ac60d5f798
Thiago Amorim <thiago.amorimsilva38@gmail.com>
2023-04-26 00:24:57 -0300

    mostra preview da tabela, loading com erro

--- ml_analytics/lib/pages/upload_file_page.dart
+++ ml_analytics/lib/pages/upload_file_page.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: avoid_print
 
+import 'dart:async';
 import 'dart:convert';
-import 'dart:typed_data';
+import 'dart:isolate';
 import 'package:csv/csv.dart';
 import 'package:excel/excel.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 
@@ -16,16 +18,55 @@ class UploadFileScreen extends StatefulWidget {
 
 class UploadFileScreenState extends State<UploadFileScreen> {
   String tableName = '';
-  String maxCols = '';
+  String maxCols = '0';
   String maxRows = '';
 
   bool isUtf8(List<int> bytes) {
     try {
       utf8.decode(bytes);
       return true;
-    } catch (e) {
+    } catch (_) {
       return false;
     }
+  }
+
+  bool isInt(String value) {
+    return int.tryParse(value) != null;
+  }
+
+  bool loading = false;
+
+  Future<void> _startIsolate(PlatformFile file) async {
+    final receivePort = ReceivePort();
+    final completer = Completer<void>();
+    final isolate = await Isolate.spawn(
+      processFileInBatches,
+      {
+        'sendPort': receivePort.sendPort,
+        'file': file,
+      },
+      debugName: 'fileProcessingIsolate',
+    );
+
+    receivePort.listen((message) {
+      if (message == 'done') {
+        receivePort.close();
+        isolate.kill(priority: Isolate.immediate);
+        completer.complete();
+        setState(() {
+          loading = false;
+        });
+      } else {
+        setState(() {
+          tableName = message['tableName'];
+          maxCols = message['maxCols'];
+          maxRows = message['maxRows'];
+          previewData = message['previewData'];
+        });
+      }
+    });
+
+    return completer.future;
   }
 
   Future<void> _selectFile() async {
@@ -38,42 +79,15 @@ class UploadFileScreenState extends State<UploadFileScreen> {
     if (pickedFiles != null) {
       final file = pickedFiles.files.single;
 
-      String? extension = file.extension;
-      Uint8List? bytes = file.bytes;
+      setState(() {
+        loading = true;
+      });
 
-      if (bytes != null) {
-        if (extension == 'xlsx') {
-          var excel = Excel.decodeBytes(bytes);
-          for (var table in excel.tables.keys) {
-            setState(() {
-              tableName = table;
-              maxCols = excel.tables[table]!.maxCols.toString();
-              maxRows = excel.tables[table]!.maxRows.toString();
-            });
-            break;
-          }
-        } else if (extension == 'csv') {
-          String csvString;
-          if (isUtf8(bytes)) {
-            csvString = utf8.decode(bytes);
-          } else {
-            csvString = const Utf8Decoder(allowMalformed: true).convert(bytes);
-          }
-
-          List<List<dynamic>> csvData =
-              const CsvToListConverter().convert(csvString);
-
-          setState(() {
-            tableName = 'CSV';
-            maxCols = csvData[0].length.toString();
-            maxRows = csvData.length.toString();
-          });
-        } else {
-          print('No valid extension or bytes is null.');
-        }
-      }
+      await _startIsolate(file);
     }
   }
+
+  List<List<dynamic>> previewData = [];
 
   @override
   Widget build(BuildContext context) {
@@ -99,12 +113,115 @@ class UploadFileScreenState extends State<UploadFileScreen> {
                   Text('Quantidade de colunas: $maxCols'),
                   const SizedBox(height: 5),
                   Text('Quantidade de linhas: $maxRows'),
+                  const SizedBox(height: 50),
                 ],
               ),
             ),
+            if (loading)
+              const CircularProgressIndicator()
+            else if (previewData.isNotEmpty &&
+                isInt(maxCols) &&
+                int.parse(maxCols) > 0)
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: DataTable(
+                  columns: List<DataColumn>.generate(
+                    int.parse(maxCols),
+                    (colIndex) => const DataColumn(label: Text('')),
+                  ),
+                  rows: previewData
+                      .map<DataRow>(
+                        (row) => DataRow(
+                          cells: row
+                              .map<DataCell>(
+                                  (cell) => DataCell(Text(cell.toString())))
+                              .toList(),
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
           ],
         ),
       ),
     );
   }
+}
+
+void processFileInBatches(Map<String, dynamic> args) async {
+  final sendPort = args['sendPort'] as SendPort;
+  final file = args['file'] as PlatformFile;
+  String? extension = file.extension;
+  Uint8List? bytes = file.bytes;
+  String tableName = '';
+  String maxCols = '0';
+  String maxRows = '';
+  List<List<dynamic>> previewData = [];
+
+  if (bytes != null) {
+    if (extension == 'xlsx') {
+      var excelData = Excel.decodeBytes(bytes);
+      for (var table in excelData.tables.keys) {
+        tableName = table;
+        maxCols = excelData.tables[table]!.maxCols.toString();
+        maxRows = excelData.tables[table]!.maxRows.toString();
+
+        int batchSize = 5;
+        int numBatches =
+            (excelData.tables[table]!.maxRows + batchSize - 1) ~/ batchSize;
+
+        for (int i = 0; i < numBatches; i++) {
+          int startRow = i * batchSize;
+          int endRow = (i + 1) * batchSize;
+          if (endRow > excelData.tables[table]!.maxRows) {
+            endRow = excelData.tables[table]!.maxRows;
+          }
+
+          previewData.addAll(excelData.tables[table]!.rows
+              .getRange(startRow, endRow)
+              .map((row) =>
+                  row.map((cell) => cell?.value?.toString() ?? '').toList())
+              .toList());
+        }
+        break;
+      }
+    } else if (extension == 'csv') {
+      String csvString;
+      if (utf8.decoder.convert(bytes).isNotEmpty) {
+        csvString = utf8.decoder.convert(bytes);
+      } else {
+        csvString = const Utf8Decoder(allowMalformed: true).convert(bytes);
+      }
+
+      List<List<dynamic>> csvData =
+          const CsvToListConverter().convert(csvString);
+      tableName = 'CSV';
+      maxCols = csvData[0].length.toString();
+      maxRows = csvData.length.toString();
+
+      int batchSize = 5;
+      int numBatches = (csvData.length + batchSize - 1) ~/ batchSize;
+
+      for (int i = 0; i < numBatches; i++) {
+        int startRow = i * batchSize;
+        int endRow = (i + 1) * batchSize;
+        if (endRow > csvData.length) {
+          endRow = csvData.length;
+        }
+
+        previewData.addAll(csvData.getRange(startRow, endRow).toList());
+      }
+    } else {
+      print('No valid extension or bytes is null.');
+    }
+  }
+
+  sendPort.send({
+    'tableName': tableName,
+    'maxCols': maxCols,
+    'maxRows': maxRows,
+    'previewData': previewData,
+  });
+
+  sendPort.send('done');
 }
